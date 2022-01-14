f33449c83aeed92cdb352f1630e66ee2a39c97d3
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-14 20:59:36 +0200

    removed footer field
    had to remove one to stop messing with the ui further

--- lib/models/footer/info_field/footer_info_field_model.dart
+++ lib/models/footer/info_field/footer_info_field_model.dart
@@ -12,5 +12,4 @@ List<FooterInfoFieldModel> footerInfoFields = [
   FooterInfoFieldModel(key: 'Address', value: 'Heraclio, Greece'),
   FooterInfoFieldModel(key: 'Phone', value: '+30 210-123-4567'),
   FooterInfoFieldModel(key: 'Email', value: 'mzamagias@gmail.com'),
-  FooterInfoFieldModel(key: 'About Us', value: '@mzamayias'),
 ];
