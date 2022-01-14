ece03ded39860301b7d8ebce20ed17d488ae3435
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-15 01:25:49 +0200

    added more routes

--- lib/routing/routes.dart
+++ lib/routing/routes.dart
@@ -1,5 +1,9 @@
-import 'package:flutter_ecommerce_website_demo/views/home/home_view.dart';
+import '../views/home/home_view.dart';
+import '../views/login/login_view.dart';
+import '../views/register/register_view.dart';
 
 Map<String, dynamic> routes = {
   'home': const HomeView(),
+  'register': const RegisterView(),
+  'login': const LoginView(),
 };
