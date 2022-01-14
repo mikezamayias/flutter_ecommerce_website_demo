11232521dd3c26528be7c276070fdf402c19c8cb
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-14 15:06:42 +0200

    center horizontally page contents

---
+++ lib/widgets/centered_view/centered_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CenteredView extends StatelessWidget {
+  final Widget child;
+
+  const CenteredView({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 69, vertical: 39),
+      alignment: Alignment.topCenter,
+      child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 1200), child: child),
+    );
+  }
+}
