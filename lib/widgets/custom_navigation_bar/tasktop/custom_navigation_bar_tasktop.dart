ebea153189128b6a1b61b21ae10231c60414b7b1
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-14 17:25:48 +0200

    renamed

--- lib/widgets/custom_navigation_bar/tasktop/custom_navigation_bar_tasktop.dart
+++ lib/widgets/custom_navigation_bar/tasktop/custom_navigation_bar_tasktop.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_website_demo/widgets/custom_navigation_bar/navigation_bar_logo.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class CustomNavigationBar extends StatelessWidget {
-  const CustomNavigationBar({Key? key}) : super(key: key);
+class CustomNavigationBarTasktop extends StatelessWidget {
+  const CustomNavigationBarTasktop({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
