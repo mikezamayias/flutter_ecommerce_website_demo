ab78ba74785cc9b31ed448eeb0a2c26e534338c8
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-14 15:34:16 +0200

    it needs work

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_website_demo/widgets/centered_view/centered_view.dart';
+import 'package:flutter_ecommerce_website_demo/widgets/phone_details/phone_details.dart';
 
 import '../../widgets/custom_navigation_bar/custom_navigation_bar.dart';
 
@@ -15,10 +17,24 @@ class _HomeViewState extends State<HomeView> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: Column(
-          children: const [
-            CustomNavigationBar(),
-          ],
+        body: SingleChildScrollView(
+          child: CenteredView(
+            child: Column(
+              children: [
+                const CustomNavigationBar(),
+                const SizedBox(height: 15),
+                Wrap(
+                  spacing: 30,
+                  runSpacing: 30,
+                  children: const [
+                    PhoneDetails(),
+                    PhoneDetails(),
+                    PhoneDetails(),
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
