a64d78366ac69b4227f40895e9bd8ad76605ea10
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-15 15:08:42 +0200

    added view blueprint

---
+++ lib/views/view_blueprint/view_blueprint.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import '../../widgets/footer/footer.dart';
+
+class ViewBlueprint extends StatefulWidget {
+  final BoxDecoration? decoration;
+  final Widget child;
+
+  const ViewBlueprint({
+    Key? key,
+    this.decoration,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  _ViewBlueprintState createState() => _ViewBlueprintState();
+}
+
+class _ViewBlueprintState extends State<ViewBlueprint> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration:
+          widget.decoration ?? const BoxDecoration(color: Color(0xFFF3F3F3)),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(60),
+              child: widget.child,
+            ),
+            const Footer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
