0a6e8083a4d42debb0cddda411e735e2ad40f952
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-15 03:04:50 +0200

    updated

--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ecommerce_website_demo/routing/routes.dart';
+import 'package:flutter_ecommerce_website_demo/views/home/home_view.dart';
+import 'package:flutter_ecommerce_website_demo/views/login/login_view.dart';
+import 'package:flutter_ecommerce_website_demo/views/register/register_view.dart';
 
-PageRoute generateRoute(RouteSettings settings) => _getPageRoute(
-      routes[settings.name]!,
+PageRoute generateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case 'home':
+      return _getPageRoute(const HomeView());
+    case 'register':
+      return _getPageRoute(const RegisterView());
+    case 'login':
+      return _getPageRoute(const LoginView());
+    default:
+      return _getPageRoute(const HomeView());
+  }
+}
+
+PageRoute _getPageRoute(Widget child) => _FadeRoute(
+      page: child,
     );
 
-PageRoute _getPageRoute(Widget child) => MaterialPageRoute(
-      builder: (context) => child,
-    );
+class _FadeRoute extends PageRouteBuilder {
+  final Widget page;
+  _FadeRoute({required this.page})
+      : super(
+          pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+          ) =>
+              page,
+          transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+              FadeTransition(opacity: animation, child: child),
+        );
+}
