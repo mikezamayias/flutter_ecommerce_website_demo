449cc5aa25a597e5083ead4a1a365cbc410b55e4
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-16 00:23:59 +0200

    refactor, "login" to "log in"

--- lib/views/log_in/log_in_view.dart
+++ lib/views/log_in/log_in_view.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_website_demo/views/view_blueprint/view_blueprint.dart';
 
-class LoginView extends StatelessWidget {
-  const LoginView({Key? key}) : super(key: key);
+class LogInView extends StatelessWidget {
+  const LogInView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const ViewBlueprint(
       child: Center(
-        child: Text('LoginView'),
+        child: Text('LogInView'),
       ),
     );
   }
