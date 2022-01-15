96f7ff451d04fb1ae98f668bdd92af0faabd8226
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-15 15:02:48 +0200

    copying and refactoring from hmu_library_website

---
+++ lib/ecommerce_website.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ExommerceWebsite extends StatefulWidget {
+  const ExommerceWebsite({Key? key}) : super(key: key);
+
+  @override
+  _ExommerceWebsiteState createState() => _ExommerceWebsiteState();
+}
+
+class _ExommerceWebsiteState extends State<ExommerceWebsite> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
