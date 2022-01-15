80fafb5bc16d956828e37a7385e91b4a92519036
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-15 21:32:39 +0200

    merged widgets

--- lib/views/view_blueprint/view_blueprint.dart
+++ lib/views/view_blueprint/view_blueprint.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
-import '../centered_view/centered_view.dart';
-import '../../widgets/footer/footer.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class ViewBlueprint extends StatefulWidget {
   final BoxDecoration? decoration;
@@ -20,23 +18,26 @@ class ViewBlueprint extends StatefulWidget {
 class _ViewBlueprintState extends State<ViewBlueprint> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: CenteredView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            widget.child,
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 21),
-              child: Divider(
-                thickness: 3,
-              ),
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) => Container(
+        padding: sizingInformation.isDesktop
+            ? const EdgeInsets.symmetric(horizontal: 90)
+            : const EdgeInsets.symmetric(horizontal: 24),
+        alignment: Alignment.topCenter,
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 1200),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                widget.child,
+                // const Padding(
+                //   padding: EdgeInsets.only(bottom: 9, top: 21),
+                //   child: Footer(),
+                // ),
+              ],
             ),
-            // const Padding(
-            //   padding: EdgeInsets.only(bottom: 9),
-            //   child: Footer(),
-            // ),
-          ],
+          ),
         ),
       ),
     );
