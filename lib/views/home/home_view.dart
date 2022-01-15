d6ef3042fa8ea979e26257339ffd6d4c050645c1
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-15 03:05:01 +0200

    refactor

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
-import '../centered_view/centered_view.dart';
 import '../../widgets/phone_details/phone_details.dart';
-import '../../widgets/footer/footer.dart';
-import '../../widgets/navigation/custom_navigation_bar.dart';
-import '../../widgets/navigation/mobile/custom_navigation_drawer.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -17,61 +13,50 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    List<PhoneDetails> phones = List.generate(
-      30,
-      (index) => const PhoneDetails(),
-    );
-    return SafeArea(
-      child: ResponsiveBuilder(
-        builder: (context, sizingInformation) => Scaffold(
-          drawer: [DeviceScreenType.mobile, DeviceScreenType.tablet]
-                  .contains(sizingInformation.deviceScreenType)
-              ? const CustomNavigationDrawer()
-              : null,
-          backgroundColor: Colors.grey[100],
-          body: SingleChildScrollView(
-            child: CenteredView(
-              child: Column(
-                children: [
-                  const CustomNavigationBar(),
-                  const SizedBox(height: 15),
-                  const Text(
-                    'Shop Phones',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 15),
-                  GridView.builder(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: phones.length,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: [
-                        DeviceScreenType.mobile,
-                        DeviceScreenType.tablet
-                      ].contains(sizingInformation.deviceScreenType)
-                          ? 1
-                          : 4,
-                      childAspectRatio: [
-                        DeviceScreenType.mobile,
-                        DeviceScreenType.tablet
-                      ].contains(sizingInformation.deviceScreenType)
-                          ? 2
-                          : 4 / 7,
-                      crossAxisSpacing: 30,
-                      mainAxisSpacing: 30,
-                    ),
-                    itemBuilder: (context, index) => const Placeholder(),
-                  ),
-                  const Footer(),
-                ],
-              ),
-            ),
-          ),
-        ),
+    return const Center(
+      child: Text(
+        'HomeView',
       ),
     );
+    // List<PhoneDetails> phones = List.generate(
+    //   30,
+    //   (index) => const PhoneDetails(),
+    // );
+    // return ResponsiveBuilder(
+    //   builder: (context, sizingInformation) => Column(
+    //     children: [
+    //       const Text(
+    //         'Shop Phones',
+    //         style: TextStyle(
+    //           fontSize: 30,
+    //           fontWeight: FontWeight.bold,
+    //         ),
+    //       ),
+    //       const SizedBox(height: 15),
+    //       GridView.builder(
+    //         shrinkWrap: true,
+    //         physics: const NeverScrollableScrollPhysics(),
+    //         itemCount: phones.length,
+    //         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+    //           crossAxisCount: [
+    //             DeviceScreenType.mobile,
+    //             DeviceScreenType.tablet
+    //           ].contains(sizingInformation.deviceScreenType)
+    //               ? 1
+    //               : 4,
+    //           childAspectRatio: [
+    //             DeviceScreenType.mobile,
+    //             DeviceScreenType.tablet
+    //           ].contains(sizingInformation.deviceScreenType)
+    //               ? 2
+    //               : 4 / 7,
+    //           crossAxisSpacing: 30,
+    //           mainAxisSpacing: 30,
+    //         ),
+    //         itemBuilder: (context, index) => const Placeholder(),
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 }
