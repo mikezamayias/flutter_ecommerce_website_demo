0b331f665fca78976df985a8e7fcb7500d0b739b
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-16 23:47:20 +0200

    removed dead code

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -21,45 +21,5 @@ class _HomeViewState extends State<HomeView> {
             : _authenticationService.currentUser!.uid,
       ),
     );
-    // List<PhoneDetails> phones = List.generate(
-    //   30,
-    //   (index) => const PhoneDetails(),
-    // );
-    // return ResponsiveBuilder(
-    //   builder: (context, sizingInformation) => Column(
-    //     children: [
-    //       const Text(
-    //         'Shop Phones',
-    //         style: TextStyle(
-    //           fontSize: 30,
-    //           fontWeight: FontWeight.bold,
-    //         ),
-    //       ),
-    //       const SizedBox(height: 15),
-    //       GridView.builder(
-    //         shrinkWrap: true,
-    //         physics: const NeverScrollableScrollPhysics(),
-    //         itemCount: phones.length,
-    //         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-    //           crossAxisCount: [
-    //             DeviceScreenType.mobile,
-    //             DeviceScreenType.tablet
-    //           ].contains(sizingInformation.deviceScreenType)
-    //               ? 1
-    //               : 4,
-    //           childAspectRatio: [
-    //             DeviceScreenType.mobile,
-    //             DeviceScreenType.tablet
-    //           ].contains(sizingInformation.deviceScreenType)
-    //               ? 2
-    //               : 4 / 7,
-    //           crossAxisSpacing: 30,
-    //           mainAxisSpacing: 30,
-    //         ),
-    //         itemBuilder: (context, index) => const Placeholder(),
-    //       ),
-    //     ],
-    //   ),
-    // );
   }
 }
