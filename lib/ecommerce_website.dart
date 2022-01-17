c3de77eff460aef1041f1881e3d5343f13f6d490
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-17 20:35:26 +0200

    refactor to include dialog manager

--- lib/ecommerce_website.dart
+++ lib/ecommerce_website.dart
@@ -8,9 +8,11 @@ import 'locator.dart';
 import 'routing/router.dart';
 import 'routing/routes.dart';
 import 'services/authentication_service.dart';
+import 'services/dialog_service.dart';
 import 'services/navigation_service.dart';
 import 'services/scaffold_service.dart';
 import 'themes/custom_theme_data.dart';
+import 'views/dialog_manager.dart';
 import 'widgets/navigation/custom_navigation_drawer.dart';
 import 'widgets/navigation/desktop_navigation_bar.dart';
 import 'widgets/navigation/mobile_navigation_bar.dart';
@@ -33,59 +35,69 @@ class _EcommerceWebsiteState extends State<EcommerceWebsite> {
         ),
       ],
       child: MaterialApp(
-          title: 'E-commerce Website',
-          debugShowCheckedModeBanner: false,
-          theme: CurrentTheme.lightTheme,
-          darkTheme: CurrentTheme.darkTheme,
-          themeMode: currentTheme.currentTheme,
-          navigatorKey: locator<NavigationService>().navigatorKey,
-          onGenerateRoute: generateRoute,
-          initialRoute: HomeViewRoute,
-          builder: (context, child) {
-            return ResponsiveBuilder(builder: (
-              BuildContext context,
-              SizingInformation sizingInformation,
-            ) {
-              return ColorfulSafeArea(
-                color: Colors.orange,
-                child: Scaffold(
-                  key: locator<ScaffoldService>().scaffoldKey,
-                  endDrawer: sizingInformation.isDesktop
-                      ? null
-                      : const CustomNavigationDrawer(),
-                  body: CustomScrollView(
-                    slivers: <Widget>[
-                      sizingInformation.isDesktop
-                          ? const DesktopNavigationBar()
-                          : const MobileNavigationBar(),
-                      SliverToBoxAdapter(
-                        child: SizedBox(
-                          height: sizingInformation.screenSize.height,
-                          child: Container(
-                            padding: sizingInformation.isDesktop
-                                ? const EdgeInsets.symmetric(horizontal: 90)
-                                : const EdgeInsets.symmetric(horizontal: 24),
-                            alignment: Alignment.topCenter,
-                            child: ConstrainedBox(
-                              constraints: const BoxConstraints(maxWidth: 1200),
-                              child: ScrollConfiguration(
-                                behavior:
-                                    ScrollConfiguration.of(context).copyWith(
-                                  scrollbars: false,
-                                  overscroll: false,
+        title: 'E-commerce Website',
+        debugShowCheckedModeBanner: false,
+        theme: CurrentTheme.lightTheme,
+        darkTheme: CurrentTheme.darkTheme,
+        themeMode: currentTheme.currentTheme,
+        navigatorKey: locator<NavigationService>().navigatorKey,
+        onGenerateRoute: generateRoute,
+        initialRoute: HomeViewRoute,
+        builder: (context, child) => Navigator(
+          key: locator<DialogService>().dialogNavigationKey,
+          onGenerateRoute: (settings) => MaterialPageRoute(
+            // builder: (context) => DialogManager(child: child!),
+            builder: (context) => ResponsiveBuilder(
+              builder: (context, sizingInformation) {
+                return DialogManager(
+                  child: ColorfulSafeArea(
+                    color: Colors.orange,
+                    child: Scaffold(
+                      key: locator<ScaffoldService>().scaffoldKey,
+                      endDrawer: sizingInformation.isDesktop
+                          ? null
+                          : const CustomNavigationDrawer(),
+                      body: CustomScrollView(
+                        slivers: <Widget>[
+                          sizingInformation.isDesktop
+                              ? const DesktopNavigationBar()
+                              : const MobileNavigationBar(),
+                          SliverToBoxAdapter(
+                            child: SizedBox(
+                              height: sizingInformation.screenSize.height,
+                              child: Container(
+                                padding: sizingInformation.isDesktop
+                                    ? const EdgeInsets.symmetric(horizontal: 90)
+                                    : const EdgeInsets.symmetric(
+                                        horizontal: 24),
+                                alignment: Alignment.topCenter,
+                                child: ConstrainedBox(
+                                  constraints:
+                                      const BoxConstraints(maxWidth: 1200),
+                                  child: ScrollConfiguration(
+                                    behavior: ScrollConfiguration.of(context)
+                                        .copyWith(
+                                      scrollbars: false,
+                                      overscroll: false,
+                                    ),
+                                    child: DialogManager(
+                                      child: child!,
+                                    ),
+                                  ),
                                 ),
-                                child: child!,
                               ),
                             ),
                           ),
-                        ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
-                ),
-              );
-            });
-          }),
+                );
+              },
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
