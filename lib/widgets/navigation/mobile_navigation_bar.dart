f682c28237b749c3b1d98016840d5c7c6a77b9cf
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-17 23:07:22 +0200

    removed user greeting from mobile nav bar
    previous (commit 5432651e ) implements #6 .

--- lib/widgets/navigation/mobile_navigation_bar.dart
+++ lib/widgets/navigation/mobile_navigation_bar.dart
@@ -1,6 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import '../../extensions/padding_extension.dart';
 import '../navigation_items/menu_navigation_item.dart';
@@ -16,8 +14,6 @@ class MobileNavigationBar extends StatefulWidget {
 class _MobileNavigationBarState extends State<MobileNavigationBar> {
   @override
   Widget build(BuildContext context) {
-    User? user = Provider.of<User?>(context);
-    bool loggedIn = user != null;
     return SliverAppBar(
       pinned: true,
       elevation: 9,
@@ -25,12 +21,7 @@ class _MobileNavigationBarState extends State<MobileNavigationBar> {
       backgroundColor: Colors.grey[100],
       centerTitle: false,
       title: const NavigationBarLogo(),
-      actions: loggedIn
-          ? [
-              const Text('User'),
-              const MenuNavigationItem().paddNavigationBarItem,
-            ]
-          : [const MenuNavigationItem().paddNavigationBarItem],
+      actions: [const MenuNavigationItem().paddNavigationBarItem],
     );
   }
 }
