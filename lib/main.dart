b415ac0bae164ed05dd022b01baf93b5893d0884
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-20 01:03:54 +0200

    sorted imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 
-import 'flutter_ecommerce_website_demo.dart';
 import 'firebase_options.dart';
-
+import 'flutter_ecommerce_website_demo.dart';
 import 'locator.dart';
 
 void main() async {
