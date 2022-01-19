e72e9a9278eb44e4d777400cb7d16936b48a1540
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-19 23:30:38 +0200

    dropped freezed

--- lib/models/user/user_model.dart
+++ lib/models/user/user_model.dart
@@ -1,12 +1,26 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
+import 'dart:convert';
 
-part 'user_model.freezed.dart';
-part 'user_model.g.dart';
+class UserModel {
+  String? uid;
+  String? firstName;
+  String? lastName;
+  String? email;
+  String? phoneNumber;
+  String? streetAddress;
+  String? userOrdersUid;
+  String? userCartUid;
+  UserModel({
+    this.uid,
+    this.firstName,
+    this.lastName,
+    this.email,
+    this.phoneNumber,
+    this.streetAddress,
+    this.userOrdersUid,
+    this.userCartUid,
+  });
 
-@Freezed()
-class UserModel with _$UserModel {
-  @JsonSerializable(explicitToJson: true)
-  factory UserModel({
+  UserModel copyWith({
     String? uid,
     String? firstName,
     String? lastName,
@@ -15,8 +29,78 @@ class UserModel with _$UserModel {
     String? streetAddress,
     String? userOrdersUid,
     String? userCartUid,
-  }) = _UserModel;
+  }) {
+    return UserModel(
+      uid: uid ?? this.uid,
+      firstName: firstName ?? this.firstName,
+      lastName: lastName ?? this.lastName,
+      email: email ?? this.email,
+      phoneNumber: phoneNumber ?? this.phoneNumber,
+      streetAddress: streetAddress ?? this.streetAddress,
+      userOrdersUid: userOrdersUid ?? this.userOrdersUid,
+      userCartUid: userCartUid ?? this.userCartUid,
+    );
+  }
 
-  factory UserModel.fromJson(Map<String, dynamic> json) =>
-      _$UserModelFromJson(json);
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'firstName': firstName,
+      'lastName': lastName,
+      'email': email,
+      'phoneNumber': phoneNumber,
+      'streetAddress': streetAddress,
+      'userOrdersUid': userOrdersUid,
+      'userCartUid': userCartUid,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      uid: map['uid'],
+      firstName: map['firstName'],
+      lastName: map['lastName'],
+      email: map['email'],
+      phoneNumber: map['phoneNumber'],
+      streetAddress: map['streetAddress'],
+      userOrdersUid: map['userOrdersUid'],
+      userCartUid: map['userCartUid'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) => UserModel.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'UserModel(uid: $uid, firstName: $firstName, lastName: $lastName, email: $email, phoneNumber: $phoneNumber, streetAddress: $streetAddress, userOrdersUid: $userOrdersUid, userCartUid: $userCartUid)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+  
+    return other is UserModel &&
+      other.uid == uid &&
+      other.firstName == firstName &&
+      other.lastName == lastName &&
+      other.email == email &&
+      other.phoneNumber == phoneNumber &&
+      other.streetAddress == streetAddress &&
+      other.userOrdersUid == userOrdersUid &&
+      other.userCartUid == userCartUid;
+  }
+
+  @override
+  int get hashCode {
+    return uid.hashCode ^
+      firstName.hashCode ^
+      lastName.hashCode ^
+      email.hashCode ^
+      phoneNumber.hashCode ^
+      streetAddress.hashCode ^
+      userOrdersUid.hashCode ^
+      userCartUid.hashCode;
+  }
 }
