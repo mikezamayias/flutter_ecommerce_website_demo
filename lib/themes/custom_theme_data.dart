dd82469abfdd8ece7e7de2f79ca6fc73b7f2cecc
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-21 21:28:01 +0200

    updated theme

--- lib/themes/custom_theme_data.dart
+++ lib/themes/custom_theme_data.dart
@@ -17,54 +17,23 @@ class CurrentTheme with ChangeNotifier {
 
   static ThemeData get darkTheme {
     return ThemeData(
-      fontFamily: GoogleFonts.firaSans().fontFamily,
+      fontFamily: GoogleFonts.mPlusRounded1c(
+        fontWeight: FontWeight.w600,
+      ).fontFamily,
       primarySwatch: Colors.orange,
       brightness: Brightness.light,
       visualDensity: VisualDensity.adaptivePlatformDensity,
-      textButtonTheme: TextButtonThemeData(
-        style: TextButton.styleFrom(
-          textStyle: GoogleFonts.mPlusRounded1c(
-            fontSize: 15,
-            fontWeight: FontWeight.bold,
-          ),
-          splashFactory: NoSplash.splashFactory,
-        ),
-      ),
     );
   }
 
   static ThemeData get lightTheme {
     return ThemeData(
-      fontFamily: GoogleFonts.firaSans().fontFamily,
+      fontFamily: GoogleFonts.mPlusRounded1c(
+        fontWeight: FontWeight.w600,
+      ).fontFamily,
       primarySwatch: Colors.orange,
       brightness: Brightness.light,
       visualDensity: VisualDensity.adaptivePlatformDensity,
-      textButtonTheme: TextButtonThemeData(
-        style: ButtonStyle(
-          textStyle: MaterialStateProperty.all<TextStyle>(
-            GoogleFonts.mPlusRounded1c(
-              fontSize: 15,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
     );
-    // return ThemeData(
-    //   visualDensity: VisualDensity.adaptivePlatformDensity,
-    //   fontFamily: GoogleFonts.montserrat().fontFamily,
-    //   brightness: _isLightTheme ? Brightness.light : Brightness.dark,
-    //   shadowColor: const Color(0xFF303030),
-    //   primaryColor: const Color(0xFFA9915D),
-    //   backgroundColor: const Color(0xFFF3F3F3),
-    //   scaffoldBackgroundColor: const Color(0xFFF3F3F3),
-    //   textButtonTheme: textButtonTheme,
-    //   textTheme: textTheme,
-    //   colorScheme: ColorScheme.fromSwatch().copyWith(
-    //     secondary: const Color(0xFF1A4859),
-    //   ),
-    //   elevatedButtonTheme: elevatedButtonThemeData,
-    //   tabBarTheme: tabBarTheme,
-    // );
   }
 }
