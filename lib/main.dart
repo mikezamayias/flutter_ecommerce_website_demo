11e33c415d461ca3cef0e73d6e9b5ffb94c9a2de
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-21 15:20:25 +0200

    added provider scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'firebase_options.dart';
 import 'flutter_ecommerce_website_demo.dart';
@@ -11,5 +12,9 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   setupLocator();
-  runApp(const FlutterEcommerceWebsiteDemo());
+  runApp(
+    const ProviderScope(
+      child: FlutterEcommerceWebsiteDemo(),
+    ),
+  );
 }
