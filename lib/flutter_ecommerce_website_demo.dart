df8762a3250f5ae38d29352449d8668c8241b5ed
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-22 11:03:00 +0200

    refactored

--- lib/flutter_ecommerce_website_demo.dart
+++ lib/flutter_ecommerce_website_demo.dart
@@ -2,14 +2,7 @@ import 'package:colorful_safe_area/colorful_safe_area.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'pages/add_phone/add_phone.dart';
-import 'pages/cart/cart_page.dart';
-import 'pages/contact_us/contact_us_page.dart';
-import 'pages/home/home_page.dart';
-import 'pages/log_in/log_in_page.dart';
-import 'pages/orders/orders_page.dart';
-import 'pages/phones/phones_page.dart';
-import 'pages/sign_up/sign_up_page.dart';
+import 'pages/pages.dart';
 import 'providers/auth_state_provider.dart';
 import 'providers/page_key_provider.dart';
 import 'theme.dart';
@@ -33,16 +26,6 @@ class _FlutterEcommerceWebsiteDemoState
 
   @override
   Widget build(BuildContext context) {
-    final _pages = {
-      '/': const HomePage(),
-      '/phones': const PhonesPage(),
-      '/signup': const SignUpPage(),
-      '/login': const LogInPage(),
-      '/contactus': const ContactUsPage(),
-      '/addphone': const AddPhonePage(),
-      '/cart': const CartPage(),
-      '/orders': const OrdersPage(),
-    };
     return MaterialApp(
       theme: theme,
       debugShowCheckedModeBanner: false,
@@ -55,7 +38,7 @@ class _FlutterEcommerceWebsiteDemoState
                 ? loggedInUserAppBarActions(context)
                 : loggedOutUserAppBarActions(context),
           ),
-          body: _pages[context.watch<PageKeyProvider>().key],
+          body: pages[context.watch<PageKeyProvider>().key],
         ),
       ),
     );
---
+++ lib/pages/pages.dart
@@ -0,0 +1,19 @@
+import 'add_phone/add_phone.dart';
+import 'cart/cart_page.dart';
+import 'contact_us/contact_us_page.dart';
+import 'home/home_page.dart';
+import 'log_in/log_in_page.dart';
+import 'orders/orders_page.dart';
+import 'phones/phones_page.dart';
+import 'sign_up/sign_up_page.dart';
+
+final pages = {
+  '/': const HomePage(),
+  '/phones': const PhonesPage(),
+  '/signup': const SignUpPage(),
+  '/login': const LogInPage(),
+  '/contactus': const ContactUsPage(),
+  '/addphone': const AddPhonePage(),
+  '/cart': const CartPage(),
+  '/orders': const OrdersPage(),
+};
