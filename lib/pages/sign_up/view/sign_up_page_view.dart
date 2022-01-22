ed4e5d8419b766352a39aa5cb2f208023817c0bb
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-23 00:03:03 +0200

    refactored

--- lib/pages/sign_up/view/sign_up_page_view.dart
+++ lib/pages/sign_up/view/sign_up_page_view.dart
@@ -15,95 +15,95 @@ class SignUpPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ViewModelBuilder<SignUpPageViewModel>.reactive(
       viewModelBuilder: () => SignUpPageViewModel(),
-      builder: (BuildContext context, SignUpPageViewModel model, Widget? child) {
+      builder: (
+        BuildContext context,
+        SignUpPageViewModel model,
+        Widget? child,
+      ) {
         return Form(
           key: locator<FormService>().signUpFormKey,
           child: ResponsiveBuilder(builder: (context, sizingInformation) {
             return AnimatedContainer(
+              alignment: Alignment.topCenter,
+              constraints: sizingInformation.isDesktop
+                  ? const BoxConstraints(maxWidth: 1200)
+                  : const BoxConstraints(),
               duration: const Duration(milliseconds: 60),
               padding: sizingInformation.isDesktop
                   ? const EdgeInsets.symmetric(horizontal: 90)
                   : const EdgeInsets.symmetric(horizontal: 30),
               child: SingleChildScrollView(
-                child: Align(
-                  alignment: Alignment.topCenter,
-                  child: ConstrainedBox(
-                    constraints: sizingInformation.isDesktop
-                        ? const BoxConstraints(maxWidth: 1200)
-                        : const BoxConstraints(),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Sign Up',
-                          style: Theme.of(context).textTheme.headline2,
-                          softWrap: true,
-                          overflow: TextOverflow.visible,
-                        ),
-                        if (sizingInformation.isDesktop)
-                          Row(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Expanded(
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: signUpFormFields.sublist(0, 5).map(
-                                    (item) {
-                                      return Padding(
-                                        padding: const EdgeInsets.symmetric(
-                                          vertical: 9,
-                                        ),
-                                        child: item,
-                                      );
-                                    },
-                                  ).toList(),
-                                ),
-                              ),
-                              const SizedBox(width: 18),
-                              Expanded(
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: signUpFormFields.sublist(5).map(
-                                    (item) {
-                                      return Padding(
-                                        padding: const EdgeInsets.symmetric(
-                                          vertical: 9,
-                                        ),
-                                        child: item,
-                                      );
-                                    },
-                                  ).toList(),
-                                ),
-                              ),
-                            ],
-                          )
-                        else
-                          ...signUpFormFields,
-                        Text(
-                          'By signing up, you agree to our Terms, Data and Cookies Policy.',
-                          style: Theme.of(context).textTheme.subtitle2,
-                          softWrap: true,
-                          overflow: TextOverflow.visible,
-                        ),
-                        const SignUpButton(),
-                      ].map(
-                        (item) {
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 9),
-                            child: item,
-                          );
-                        },
-                      ).toList(),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Sign Up',
+                      style: Theme.of(context).textTheme.headline2,
+                      softWrap: true,
+                      overflow: TextOverflow.visible,
                     ),
-                  ),
+                    if (sizingInformation.isDesktop)
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: signUpFormFields.sublist(0, 5).map(
+                                (item) {
+                                  return Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      vertical: 9,
+                                    ),
+                                    child: item,
+                                  );
+                                },
+                              ).toList(),
+                            ),
+                          ),
+                          const SizedBox(width: 18),
+                          Expanded(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: signUpFormFields.sublist(5).map(
+                                (item) {
+                                  return Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      vertical: 9,
+                                    ),
+                                    child: item,
+                                  );
+                                },
+                              ).toList(),
+                            ),
+                          ),
+                        ],
+                      )
+                    else
+                      ...signUpFormFields,
+                    Text(
+                      'By signing up, you agree to our Terms, Data and Cookies Policy.',
+                      style: Theme.of(context).textTheme.subtitle2,
+                      softWrap: true,
+                      overflow: TextOverflow.visible,
+                    ),
+                    const SignUpButton(),
+                  ].map(
+                    (item) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 9),
+                        child: item,
+                      );
+                    },
+                  ).toList(),
                 ),
               ),
             );
