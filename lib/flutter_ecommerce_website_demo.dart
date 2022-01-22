6e5dfab4d5e08abec19db90c524e1544245d2c8f
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-22 21:01:25 +0200

    refactored ui
    Resolves #9

--- lib/flutter_ecommerce_website_demo.dart
+++ lib/flutter_ecommerce_website_demo.dart
@@ -1,12 +1,17 @@
 import 'package:colorful_safe_area/colorful_safe_area.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_website_demo/widgets/drawer/mobile_drawer.dart';
 import 'package:provider/provider.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
+import 'locator.dart';
 import 'pages/pages.dart';
-import 'providers/auth_state_provider.dart';
 import 'providers/page_key_provider.dart';
+import 'providers/sizing_information_provider.dart';
+import 'services/scaffold_service.dart';
 import 'theme.dart';
-import 'widgets/app_bar/app_bar_user_authentication.dart';
+import 'widgets/app_bar_actions/desktop_navigation/desktop_navigation_app_bar_actions.dart';
+import 'widgets/app_bar_actions/mobile_navigation/mobile_navigation_app_bar_actions.dart';
 
 class FlutterEcommerceWebsiteDemo extends StatefulWidget {
   const FlutterEcommerceWebsiteDemo({Key? key}) : super(key: key);
@@ -29,17 +34,30 @@ class _FlutterEcommerceWebsiteDemoState
     return MaterialApp(
       theme: theme,
       debugShowCheckedModeBanner: false,
-      home: ColorfulSafeArea(
-        color: Colors.orange,
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.white,
-            actions: context.watch<AuthStateProvider>().authState
-                ? loggedInUserAppBarActions(context)
-                : loggedOutUserAppBarActions(context),
-          ),
-          body: pages[context.watch<PageKeyProvider>().key],
-        ),
+      home: ResponsiveBuilder(
+        builder: (BuildContext context, SizingInformation sizingInformation) {
+          Provider.of<SizingInformationProvider>(context, listen: false)
+              .sizingInformation = sizingInformation;
+          return ColorfulSafeArea(
+            color: Colors.orange,
+            child: Scaffold(
+              key: locator<ScaffoldService>().scaffoldKey,
+              endDrawer: context
+                      .watch<SizingInformationProvider>()
+                      .sizingInformation
+                      .isDesktop
+                  ? null
+                  : const MobileDrawer(),
+              appBar: AppBar(
+                backgroundColor: Colors.white,
+                actions: sizingInformation.isDesktop
+                    ? desktopNavigationAppBarActions(context)
+                    : mobileNavigationAppBarActions(context),
+              ),
+              body: pages[context.watch<PageKeyProvider>().key],
+            ),
+          );
+        },
       ),
     );
   }
