6a0b75b8deb4c671ff1b0af946d860a91e4c7b02
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-23 00:11:16 +0200

    removed dialog and navigation services from locator
    they're already implented in stacked services

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,14 +1,11 @@
 import 'package:get_it/get_it.dart';
 
 import 'services/form_service.dart';
-import 'services/navigation_service.dart';
 import 'services/scaffold_service.dart';
 
 GetIt locator = GetIt.instance;
 
 void setupLocator() {
-  locator.registerLazySingleton(() => NavigationService());
-  // locator.registerLazySingleton(() => DialogService());
   // locator.registerLazySingleton(() => AuthenticationService());
   locator.registerLazySingleton(() => ScaffoldService());
   locator.registerLazySingleton(() => FormService());
