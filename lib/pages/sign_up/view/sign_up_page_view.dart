49f6909e43d3c966b9a8038f4990d93e9073eeb9
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-23 01:06:04 +0200

    refactor

--- lib/pages/sign_up/view/sign_up_page_view.dart
+++ lib/pages/sign_up/view/sign_up_page_view.dart
@@ -23,90 +23,92 @@ class SignUpPageView extends StatelessWidget {
         return Form(
           key: locator<FormService>().signUpFormKey,
           child: ResponsiveBuilder(builder: (context, sizingInformation) {
-            return AnimatedContainer(
-              alignment: Alignment.topCenter,
-              constraints: sizingInformation.isDesktop
-                  ? const BoxConstraints(maxWidth: 1200)
-                  : const BoxConstraints(),
-              duration: const Duration(milliseconds: 60),
-              padding: sizingInformation.isDesktop
-                  ? const EdgeInsets.symmetric(horizontal: 90)
-                  : const EdgeInsets.symmetric(horizontal: 30),
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Sign Up',
-                      style: Theme.of(context).textTheme.headline2,
-                      softWrap: true,
-                      overflow: TextOverflow.visible,
-                    ),
-                    if (sizingInformation.isDesktop)
-                      Row(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: signUpFormFields.sublist(0, 5).map(
-                                (item) {
-                                  return Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                      vertical: 9,
-                                    ),
-                                    child: item,
-                                  );
-                                },
-                              ).toList(),
+            return SingleChildScrollView(
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: AnimatedContainer(
+                  constraints: sizingInformation.isDesktop
+                      ? const BoxConstraints(maxWidth: 1200)
+                      : const BoxConstraints(),
+                  duration: const Duration(milliseconds: 60),
+                  padding: sizingInformation.isDesktop
+                      ? const EdgeInsets.symmetric(horizontal: 90)
+                      : const EdgeInsets.symmetric(horizontal: 30),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Sign Up',
+                        style: Theme.of(context).textTheme.headline2,
+                        softWrap: true,
+                        overflow: TextOverflow.visible,
+                      ),
+                      if (sizingInformation.isDesktop)
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: signUpFormFields.sublist(0, 5).map(
+                                  (item) {
+                                    return Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                        vertical: 9,
+                                      ),
+                                      child: item,
+                                    );
+                                  },
+                                ).toList(),
+                              ),
                             ),
-                          ),
-                          const SizedBox(width: 18),
-                          Expanded(
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: signUpFormFields.sublist(5).map(
-                                (item) {
-                                  return Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                      vertical: 9,
-                                    ),
-                                    child: item,
-                                  );
-                                },
-                              ).toList(),
+                            const SizedBox(width: 18),
+                            Expanded(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: signUpFormFields.sublist(5).map(
+                                  (item) {
+                                    return Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                        vertical: 9,
+                                      ),
+                                      child: item,
+                                    );
+                                  },
+                                ).toList(),
+                              ),
                             ),
-                          ),
-                        ],
-                      )
-                    else
-                      ...signUpFormFields,
-                    Text(
-                      'By signing up, you agree to our Terms, Data and Cookies Policy.',
-                      style: Theme.of(context).textTheme.subtitle2,
-                      softWrap: true,
-                      overflow: TextOverflow.visible,
-                    ),
-                    BusyButton(
-                      title: 'Sign Up',
-                      onPressed: () {},
-                    ),
-                  ].map(
-                    (item) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 9),
-                        child: item,
-                      );
-                    },
-                  ).toList(),
+                          ],
+                        )
+                      else
+                        ...signUpFormFields,
+                      Text(
+                        'By signing up, you agree to our Terms, Data and Cookies Policy.',
+                        style: Theme.of(context).textTheme.subtitle2,
+                        softWrap: true,
+                        overflow: TextOverflow.visible,
+                      ),
+                      BusyButton(
+                        title: 'Sign Up',
+                        onPressed: () {},
+                      ),
+                    ].map(
+                      (item) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 9),
+                          child: item,
+                        );
+                      },
+                    ).toList(),
+                  ),
                 ),
               ),
             );
