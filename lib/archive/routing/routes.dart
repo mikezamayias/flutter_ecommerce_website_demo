921973296d643524291a6c7366605c1cbed55019
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-22 10:38:47 +0200

    removed old code

--- lib/archive/routing/routes.dart
+++
@@ -1,9 +0,0 @@
-// ignore_for_file: non_constant_identifier_names, constant_identifier_names
-
-const String shopRoute = '/shop';
-const String signUpRoute = '/signup';
-const String loginRoute = '/login';
-const String cartRoute = '/cart';
-const String ordersRoute = '/orders';
-const String contactRoute = '/contact';
-const String addPhoneRoute = '/addphone';
--- lib/archive/services/scaffold_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ScaffoldService {
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-
-  ScaffoldState? get currentState => _scaffoldKey.currentState;
-
-  GlobalKey<ScaffoldState> get scaffoldKey => _scaffoldKey;
-}
