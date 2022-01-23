9e08ebcd17db615e4ed17d58ced335736efa62d0
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-23 20:42:19 +0200

    theres a functioning user header in the drawer now

--- lib/widgets/drawer/mobile_drawer.dart
+++ lib/widgets/drawer/mobile_drawer.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../../locator.dart';
+import '../../models/user/user_model.dart';
 import '../../services/authentication_service.dart';
+import '../../services/firestore_service.dart';
 import '../app_bar_actions/desktop_navigation/desktop_navigation_app_bar_actions.dart';
 
 class MobileDrawer extends StatelessWidget {
@@ -9,18 +12,48 @@ class MobileDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final user = locator<AuthenticationService>().currentUser;
+    User? _user = locator<AuthenticationService>().currentUser;
+    print('_user: ${_user.toString()}');
     return Drawer(
       child: ListView(
         children: [
-          DrawerHeader(
-            child: user == null
-                ? Text('Hello', style: Theme.of(context).textTheme.headline6)
-                : Text(
-                    'Hello,\n${user.uid}',
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-          ),
+          _user == null
+              ? const DrawerHeader(child: SizedBox())
+              : StreamBuilder<UserModel?>(
+                  stream: locator<FirestoreService>().streamUser(_user.uid),
+                  builder: (
+                    BuildContext context,
+                    AsyncSnapshot<UserModel?> snapshot,
+                  ) {
+                    if (snapshot.hasData) {
+                      final _userModel = snapshot.data;
+                      if (_userModel != null) {
+                        return UserAccountsDrawerHeader(
+                          accountName: Text(
+                            '${_userModel.firstName} ${_userModel.lastName}',
+                          ),
+                          accountEmail: Text(_userModel.email),
+                        );
+                      } else {
+                        return const DrawerHeader(
+                          child: Text('Loading'),
+                        );
+                      }
+                    } else if (snapshot.hasError) {
+                      debugPrint(snapshot.error.toString());
+                      return const Center(
+                        child: Icon(
+                          Icons.error_outline_rounded,
+                          color: Colors.red,
+                        ),
+                      );
+                    } else {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  },
+                ),
           ...desktopNavigationAppBarActions(context).map((action) {
             return SizedBox(
               height: 60,
