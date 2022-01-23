74ef4c6160c651e31166bc5e0619bb1acd33f49e
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-23 13:46:20 +0200

    refactor

--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -2,7 +2,7 @@ import 'add_phone/add_phone_page_view.dart';
 import 'cart/cart_page_view.dart';
 import 'contact_us/contact_us_page_view.dart';
 import 'home/home_page_view.dart';
-import 'log_in/log_in_page_view.dart';
+import 'log_in/view/log_in_page_view.dart';
 import 'orders/orders_page_view.dart';
 import 'phones/phones_page_view.dart';
 import 'sign_up/view/sign_up_page_view.dart';
